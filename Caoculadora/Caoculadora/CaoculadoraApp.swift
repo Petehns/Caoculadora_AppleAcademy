3ebe959cb0be436bfd0f1e754a16a652f038c536
Pedro Henrique Nunes da Silveira Bezerra <pedronunesufpe@gmail.com>
2024-05-07 11:33:18 -0300

    cria projeto da caoculadora no Xcode

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Pedro Henrique Nunes da Silveira Bezerra on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
