4b44b5991bc95bdc0cf8ff9682c7cda69e3be92b
Pedro Henrique Nunes da Silveira Bezerra <pedronunesufpe@gmail.com>
2024-05-09 11:53:35 -0300

    imagem adicionada

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -8,15 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var years: Int?
+    @State var months: Int?
+    @State var result: Int?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Qual a idade do seu cão?")
+            
+            Text("Anos")
+            TextField("Quantos anos completos tem seu cão?",
+                      value: $years,
+                      format: .number)
+            Text("Meses")
+            TextField("Quantos meses tem seu cão?", value: $months, format: .number)
+            Text("Porte")
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+            }else{
+            }
+            Image(ImageResource.clarinha)
+            
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 198, maxHeight: 198.4)
+                .padding(.horizontal, 70)
         }
+            Button("Cãocular"){
+                print("cãocular")
+                result = 23
+            }
+//            .frame(maxWidth: .infinity)
+//            frame(height)
+        }
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
         .padding()
     }
+    
 }
 
 #Preview {
